92537976464578c8965ad01e3324cf594f43cd9a
Next Suite <ivangomezfm@gmail.com>
2026-02-23 18:20:10 +0100

    prueba

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: [
-    "192.168.0.35",
-    "poor-snakes-play.loca.lt",
-  ],
 };
 
 export default nextConfig;
